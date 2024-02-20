270b6944ffa7f29d8f722304a082b42a0b9686d9
Esperanza <wildfrenzy0432@gmail.com>
2024-02-20 04:45:52 +0100

    Sully is working, but it replaces sprintf to builtin :c

--- Sully/C/sully.c
+++ Sully/C/sully.c
@@ -1,34 +1,12 @@
-//test from github mobile
 #include <stdio.h>
 #include <stdlib.h>
 int i = 5;
-/*
-//for ref
-#define MAIN(x) int main() {puts("#include <stdio.h>\n\n#define MAIN(x) "x"\n#define ADD(s) STRINGIFY(s)\n#define STRINGIFY(s) #s\nMAIN(ADD(MAIN(x)))");}
+char num[11];
+char file[18];
+char syscompile[50];
+char syscall[25];
+
+#define MAIN(x) int main() {if (i > 0){ FILE *fp;sprintf(file,"Sully_%d.c", --i);fp = fopen(file, "w");if(fp == NULL){perror("Error opening file");return 1;}sprintf(num, "%d", i);fprintf(fp, "#include <stdio.h>\n#include <stdlib.h>\nint i = %d;\nchar num[11];\nchar file[18];\nchar syscompile[50];\nchar syscall[25];\n\n#define MAIN(x) %s\n#define ADD(s) STRINGIFY(s)\n#define STRINGIFY(s) #s\nMAIN(ADD(MAIN(x)))\n", i, x);fclose(fp);sprintf(syscompile, "cc -O0 Sully_%d.c -o Sully_%d", i, i);sprintf(syscall, "./Sully_%d", i);system(syscompile);system(syscall);};}
 #define ADD(s) STRINGIFY(s)
 #define STRINGIFY(s) #s
 MAIN(ADD(MAIN(x)))
-*/
-int main() {
-if (i > 0 && i < 6){
-    FILE *fp;
-    char filename[10];
-    sprintf(filename, "Sully_%d.c", i);
-    fp = fopen(filename, "w");
-    if(fp == NULL){
-        perror("Error opening file");
-        return 1;
-    }
-    //add program code in this fputs
-    fputs("#include <stdio.h>\nint i = "--i"\n//code here", fp);
-    fclose(fp);
-    //there must be a better way to do this ⬇️
-    char syscompile[27];
-    sprintf(syscompile, "clang Sully_%d.c -o Sully_%d", i, i);
-    system(syscompile);
-    char syscall[10];
-    sprintf(syscall, "./Sully_%d", i);
-    system(syscall);
-    }
-    return 0;
-}
