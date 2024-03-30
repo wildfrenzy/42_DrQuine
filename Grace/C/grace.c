450e0367de19d74b727577b54555460028c8edd4
Esperanza <wildfrenzy0432@gmail.com>
2024-03-30 01:13:42 +0100

    grace fix: forgot to add write into a file. oops

--- Grace/C/grace.c
+++ Grace/C/grace.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-//one comment
-#define MAIN(x) int main() {puts("#include <stdio.h>\n//one comment\n#define MAIN(x) "x"\n#define ADD(s) STRINGIFY(s)\n#define STRINGIFY(s) #s\nMAIN(ADD(MAIN(x)))");}
+/*
+	To be or not to be...
+*/
+#define MAIN(x) int main() {FILE *fp;fp = fopen("Grace_kid.c", "w");fputs("#include <stdio.h>\n/*\n	To be or not to be...\n*/\n#define MAIN(x) "x"\n#define ADD(s) STRINGIFY(s)\n#define STRINGIFY(s) #s\nMAIN(ADD(MAIN(x)))\n", fp);fclose(fp);}
 #define ADD(s) STRINGIFY(s)
 #define STRINGIFY(s) #s
 MAIN(ADD(MAIN(x)))
