f53008176cd80b9777d1c6d7fd4dd402ffee0340
Esperanza <wildfrenzy0432@gmail.com>
2024-04-02 10:02:47 +0200

    Colleen upd according to subject requirements

--- Colleen/Python/Colleen.py
+++ Colleen/Python/Colleen.py
@@ -1,2 +1,7 @@
-quine: str = 'quine: str = %r%cprint(quine %% (quine, 10))'
-print(quine % (quine, 10))
+#  outside of my program
+def myquine() -> None:
+    quine: str = "#  outside of my program%cdef myquine() -> None:%c    quine: str = %r%c    print(quine %% (10, 10, quine, 10, 10, 10, 10))%cif __name__ == '__main__':%c    # in main%c    myquine()"
+    print(quine % (10, 10, quine, 10, 10, 10, 10))
+if __name__ == '__main__':
+    # in main
+    myquine()
